5559f1f12789a4ebe282a52e864b1125ecda9455
sanjay1994singh <sanjaypython22@gmail.com>
2026-09-06 23:31:26 +0530

    p

--- tenants/test_public_branding.py
+++ tenants/test_public_branding.py
@@ -18,3 +18,17 @@ class PublicBrandingTests(SimpleTestCase):
     def test_missing_channel_never_exposes_publisher(self):
         tenant = Tenant(business_name='  ', publication_name='Private publisher')
         self.assertEqual(tenant.public_name, 'News')
+
+
+    def test_workspace_base_and_reporter_billing_branding(self):
+        from types import SimpleNamespace
+        from django.contrib.auth.models import AnonymousUser
+        tenant = Tenant(business_name='National 24', publication_name='Shravya')
+        request = SimpleNamespace(tenant=tenant, user=AnonymousUser(), path='/cms/')
+        for name in ['news/article_dashboard.html', 'tenants/reporter_list.html', 'subscriptions/billing_dashboard.html']:
+            with self.subTest(template=name):
+                html = render_to_string(name, {'tenant':tenant, 'request':request, 'user':request.user, 'is_tenant_domain':True})
+                self.assertEqual(html.count('class="channel-identity"'), 2)
+                self.assertNotIn('Shravya', html)
+                self.assertNotIn('class="tenant-brand-mark"', html)
+                self.assertIn('National 24', html)
